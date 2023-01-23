e4955e18823e1311a1f99ff37cfa31b976c50007
ArsenKoch <arsenkocaran@icloud.com>
2023-01-23 23:01:42 +0300

    Create UserAdapter.kt for RecyclerView

---
+++ app/src/main/java/com/example/recyclerviewtask/UserAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.recyclerviewtask
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.recyclerviewtask.databinding.ItemUserBinding
+import com.example.recyclerviewtask.module.User
+
+class UserAdapter : Adapter<UserAdapter.UserViewHolder>() {
+
+    private var users: List<User> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class UserViewHolder(
+        val binding: ItemUserBinding
+    ) : ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemUserBinding.inflate(inflater, parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = users[position]
+        with(holder.binding) {
+            tvCompany.text = user.company
+            tvName.text = user.name
+            if (user.photo.isNotBlank()) {
+                Glide.with(ivPhoto.context)
+                    .load(user.photo)
+                    .circleCrop()
+                    .placeholder(R.drawable.ic_person)
+                    .error(R.drawable.ic_person)
+                    .into(ivPhoto)
+            } else {
+                ivPhoto.setImageResource(R.drawable.ic_person)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = users.size
+}
