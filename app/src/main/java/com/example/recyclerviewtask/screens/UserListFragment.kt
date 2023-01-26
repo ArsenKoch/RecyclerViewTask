6f3e76bfb7420369fa46f35177938673316058ab
ArsenKoch <arsenkocaran@icloud.com>
2023-01-26 21:49:22 +0300

    Fill out the class UserListFragment.kt

--- app/src/main/java/com/example/recyclerviewtask/screens/UserListFragment.kt
+++ app/src/main/java/com/example/recyclerviewtask/screens/UserListFragment.kt
@@ -1,4 +1,50 @@
 package com.example.recyclerviewtask.screens
 
-class UserListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewtask.UserActionListener
+import com.example.recyclerviewtask.UserAdapter
+import com.example.recyclerviewtask.databinding.FragmentUserListBinding
+import com.example.recyclerviewtask.module.User
+
+class UserListFragment : Fragment() {
+
+    private lateinit var binding: FragmentUserListBinding
+    private lateinit var adapter: UserAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUserListBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        adapter = UserAdapter(object : UserActionListener {
+            override fun onUserMove(user: User, moveBy: Int) {
+            }
+
+            override fun onUserDelete(user: User) {
+            }
+
+            override fun onUserDetails(user: User) {
+            }
+
+            override fun onUserFire(user: User) {
+            }
+        })
+
+        val layoutManager = LinearLayoutManager(requireContext())
+
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        return binding.root
+    }
 }
