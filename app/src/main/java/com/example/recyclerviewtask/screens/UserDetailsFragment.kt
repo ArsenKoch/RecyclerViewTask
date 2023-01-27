47bed56fe753a24df2ced6141ebfcbc80e1c59c1
ArsenKoch <arsenkocaran@icloud.com>
2023-01-27 20:45:03 +0300

    Realize a new class UserDetailsFragment.kt. Part 1

--- app/src/main/java/com/example/recyclerviewtask/screens/UserDetailsFragment.kt
+++ app/src/main/java/com/example/recyclerviewtask/screens/UserDetailsFragment.kt
@@ -1,4 +1,66 @@
 package com.example.recyclerviewtask.screens
 
-class UserDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.example.recyclerviewtask.R
+import com.example.recyclerviewtask.UserDetailsViewModel
+import com.example.recyclerviewtask.databinding.FragmentUserDetailsBinding
+import com.example.recyclerviewtask.factory
+
+class UserDetailsFragment : Fragment() {
+
+    private val viewModel: UserDetailsViewModel by viewModels { factory() }
+    private lateinit var binding: FragmentUserDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.loadUser(requireArguments().getLong(ARG_USER_ID))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
+
+        viewModel.userDetails.observe(viewLifecycleOwner, Observer {
+            binding.tvUserDetailsFragment.text = it.user.name
+            if (it.user.photo.isNotBlank()) {
+                Glide.with(this)
+                    .load(it.user.photo)
+                    .circleCrop()
+                    .into(binding.ivPhotoFragment)
+            } else {
+                Glide.with(this)
+                    .load(R.drawable.ic_person)
+                    .into(binding.ivPhotoFragment)
+            }
+            binding.tvUserDetailsFragment.text = it.details
+        })
+
+        binding.btnDelete.setOnClickListener {
+            viewModel.deleteUser()
+            //todo
+        }
+
+        return binding.root
+    }
+
+    companion object {
+        private const val ARG_USER_ID = "ARG_USER_ID"
+
+        fun newInstance(userId: Long): UserDetailsFragment {
+            val fragment = UserDetailsFragment()
+            fragment.arguments = bundleOf(ARG_USER_ID to userId)
+            return fragment
+        }
+    }
 }
