fffe4599e2e230ff65b8945cece50104f4dfe69e
ArsenKoch <arsenkocaran@icloud.com>
2023-01-27 20:32:18 +0300

    Realize a method loadUser() in UserDetailsViewModel.kt

--- app/src/main/java/com/example/recyclerviewtask/UserDetailsViewModel.kt
+++ app/src/main/java/com/example/recyclerviewtask/UserDetailsViewModel.kt
@@ -3,22 +3,26 @@ package com.example.recyclerviewtask
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.recyclerviewtask.module.User
 import com.example.recyclerviewtask.module.UserService
 
 class UserDetailsViewModel(
     private val userService: UserService
 ) : ViewModel() {
 
-    private val _userDetails = MutableLiveData<User>()
-    val userDetails: LiveData<User> = _userDetails
+    private val _userDetails = MutableLiveData<UserDetails>()
+    val userDetails: LiveData<UserDetails> = _userDetails
 
     fun loadUser(id: Long) {
-
+        if (_userDetails.value != null) return
+        try {
+            _userDetails.value = userService.getUserById(id)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     fun deleteUser() {
         val user = this.userDetails.value ?: return
-        userService.deleteUser(user)
+        userService.deleteUser(user.user)
     }
 }
