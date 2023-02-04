5323dce95c30eee5417c9ec85320f1e4c6a8740a
ArsenKoch <arsenkocaran@icloud.com>
2023-02-04 22:35:20 +0300

    Realize a class BaseViewModel.kt

--- app/src/main/java/com/example/recyclerviewtask/BaseViewModel.kt
+++ app/src/main/java/com/example/recyclerviewtask/BaseViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.recyclerviewtask
 
 import androidx.lifecycle.ViewModel
+import com.example.recyclerviewtask.tasks.Task
 
-class BaseViewModel: ViewModel() {
+class BaseViewModel : ViewModel() {
+
+    private val task = mutableListOf<Task<*>>()
 
     override fun onCleared() {
         super.onCleared()
+        task.forEach { it.cancel() }
+    }
+
+    fun <T> Task<T>.autoCancel() {
+        task.add(this)
     }
 }
