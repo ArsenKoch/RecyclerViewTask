d1aee9ced267adad8f285319b300a85aef9c077c
ArsenKoch <arsenkocaran@icloud.com>
2023-02-04 22:32:43 +0300

    Create a new class

---
+++ app/src/main/java/com/example/recyclerviewtask/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.recyclerviewtask
+
+import androidx.lifecycle.ViewModel
+
+class BaseViewModel: ViewModel() {
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
